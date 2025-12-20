e82f415af42eae5d08343149cb058cc6790cb8e3
James Park <jmpark.eng36@gmail.com>
2025-12-19 21:25:35 -0500

    feat: CLI entry point

---
+++ cmd/root.go
@@ -0,0 +1,12 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+var rpcURL string
+var rootCmd = &cobra.Command{Use: "msig", Short: "DeFi Multisig Toolkit v1"}
+func Execute() { if err := rootCmd.Execute(); err != nil { fmt.Fprintln(os.Stderr, err); os.Exit(1) } }
+func init() { rootCmd.PersistentFlags().StringVar(&rpcURL, "rpc", os.Getenv("RPC_URL"), "RPC endpoint") }
