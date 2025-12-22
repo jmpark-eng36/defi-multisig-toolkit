63f87890ae4ed024d8ec8285d801e5166dd6790e
James Park <jmpark.eng36@gmail.com>
2025-12-22 15:04:00 -0500

    feat: Safe contract bindings

---
+++ pkg/safe/safe.go
@@ -0,0 +1,7 @@
+package safe
+
+import "github.com/ethereum/go-ethereum/common"
+
+type Safe struct { Address common.Address }
+type SafeInfo struct { Threshold uint64; Nonce uint64 }
+const MaxRetries = 6
