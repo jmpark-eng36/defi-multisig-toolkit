500f7d72e41c53b19039ca982de3593a635ef09f
James Park <jmpark.eng36@gmail.com>
2025-12-22 14:29:12 -0500

    feat: root command

---
+++ cmd/inspect.go
@@ -0,0 +1,10 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
+
+var inspectCmd = &cobra.Command{Use: "inspect [addr]", Short: "Inspect Safe", Args: cobra.ExactArgs(1), RunE: func(c *cobra.Command, args []string) error { fmt.Println("inspecting", args[0]); return nil }}
+func init() { rootCmd.AddCommand(inspectCmd) }
+// v2
