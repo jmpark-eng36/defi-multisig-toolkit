05a272a2b2550be21d3bcbbc6c6c39e23c39b166
James Park <jmpark.eng36@gmail.com>
2025-12-22 21:29:04 -0500

    feat: inspect command

---
+++ pkg/safe/utils.go
@@ -0,0 +1,9 @@
+package safe
+
+import "math/big"
+
+func WeiToEth(wei *big.Int) float64 {
+	f, _ := new(big.Float).Quo(new(big.Float).SetInt(wei), new(big.Float).SetInt(big.NewInt(1e18))).Float64()
+	return f
+}
+// v4
