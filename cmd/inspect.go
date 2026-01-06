6bb9aaf210cf4cb3af52ab2a2e2f2c646ece22a0
James Park <jmpark.eng36@gmail.com>
2026-01-05 21:33:39 -0500

    docs: update install

--- cmd/inspect.go
+++ cmd/inspect.go
@@ -7,4 +7,4 @@ import (
 
 var inspectCmd = &cobra.Command{Use: "inspect [addr]", Short: "Inspect Safe", Args: cobra.ExactArgs(1), RunE: func(c *cobra.Command, args []string) error { fmt.Println("inspecting", args[0]); return nil }}
 func init() { rootCmd.AddCommand(inspectCmd) }
-// v8
+// v14
