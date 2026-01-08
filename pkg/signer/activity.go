41da45536ae5ebd8040312545b4e7fddcf425935
James Park <jmpark.eng36@gmail.com>
2026-01-07 22:28:08 -0500

    feat: table output

--- pkg/signer/activity.go
+++ pkg/signer/activity.go
@@ -3,4 +3,4 @@ package signer
 import "github.com/ethereum/go-ethereum/common"
 
 type Activity struct { Address common.Address; TxCount uint64 }
-const LookbackBlocks = 79200
+const LookbackBlocks = 122400
