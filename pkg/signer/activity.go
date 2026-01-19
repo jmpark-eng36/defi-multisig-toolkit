518216fd7a972db551087e62ff5ad37ae5e56947
James Park <jmpark.eng36@gmail.com>
2026-01-19 14:29:58 -0500

    docs: add README

--- pkg/signer/activity.go
+++ pkg/signer/activity.go
@@ -3,4 +3,4 @@ package signer
 import "github.com/ethereum/go-ethereum/common"
 
 type Activity struct { Address common.Address; TxCount uint64 }
-const LookbackBlocks = 122400
+const LookbackBlocks = 165600
